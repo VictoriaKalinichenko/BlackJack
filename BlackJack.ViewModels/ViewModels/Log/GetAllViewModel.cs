d63016a6e4d2f2b7f61d593d09668796dc854313
Викуська Калиниченко <viktoriamay0130@gmail.com>
2018-09-27 15:15:16 +0300

    Commit: Code refactoring

---
+++ BlackJack.ViewModels/ViewModels/Log/GetAllViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlackJack.ViewModels.ViewModels.Log
+{
+    public class GetAllViewModel
+    {
+        public long Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public long GameId { get; set; }
+        public string Message { get; set; }
+    }
+}

7a7cf7d9d2906261c62c2bb190556c5935183995
Викуська Калиниченко <viktoriamay0130@gmail.com>
2018-11-09 11:35:32 +0200

    Commit: Code refactoring

--- BlackJack.DataAccess/Repositories/PlayerRepository.cs
+++ BlackJack.DataAccess/Repositories/PlayerRepository.cs
@@ -1,9 +1,6 @@
 ﻿using BlackJack.DataAccess.Repositories.Interfaces;
 using BlackJack.Entities.Entities;
-using BlackJack.Entities.Enums;
-using Dapper;
 using System.Collections.Generic;
-using System.Data;
 using System.Data.Common;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
